c53f318789a66597df7c0bb5944d98b5b3988506
jadashi97 <njadauniversal23@yahoo.com>
2023-03-25 16:29:01 -0500

    solve leetcode problem that involves finding the pivot

--- 01leetcode/jsAnswers/leetCode75Answers.js
+++ 01leetcode/jsAnswers/leetCode75Answers.js
@@ -1,15 +1,51 @@
 // Qtn.1 Given the head of a singly linked list, reverse the list, and return the reversed list.
-var reverseList = function(head) {
-    let prev = null;
-    let current = head;
-    let next = null;
+// var reverseList = function(head) {
+//     let prev = null;
+//     let current = head;
+//     let next = null;
 
-    while (current !== null) {
-        next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
+//     while (current !== null) {
+//         next = current.next;
+//         current.next = prev;
+//         prev = current;
+//         current = next;
+//     }
+
+//   return prev;
+// };
+
+// Q2. 1480. Running Sum of 1d Array
+var runningTotal = (nums)=>{
+  let total = " ";
+  result = [ ];
+
+  for(let i = 0; i < nums.length; i++){
+    total = total + nums[i];
+    resullt = push(total);
+  }
+  return result;
+}
+
+runningTotal([1,2,3,4]);
+
+// Q3. 724. Find Pivot Index
+let totalSum = 0;
+
+let pivotIndex = (nums) => {
+  for(let i = 0; i < nums.length; i++){
+    totalSum += nums[i];
+  }
+
+  let leftSum = 0;
+
+  for(let i = 0; i < nums.length; i++){
+    let rightSum  = totalSum - leftSum - nums[i];
+
+    if(rightSum === leftSum){
+      return i
     }
+    leftSum += nums[i]
+  }
+return -1
 
-  return prev;
-};
+}
